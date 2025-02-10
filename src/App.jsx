654afbf54094c07d11694641d9fc92475ad5d46b
Yashraj360 <yashraj.sahu.jobsdoor360@gmail.com>
2025-02-10 18:51:48 +0530

    stopped button from unneccesary re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import './App.css'
-import {count_State} from "../src/Store/Atoms/CounterAtom"
-import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil"
+import "./App.css";
+import { count_State } from "../src/Store/Atoms/CounterAtom";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 function App() {
-  
   return (
-    <>   
-        <Count />    
+    <>
+      <Count />
     </>
-  )
+  );
 }
 
-function Count(){
+function Count() {
   console.log("re"); // now it is not re-rendering
-    return (<>
-     <Buttons />
-     <CountRenderer />
-  </>)
+  return (
+    <>
+      <Buttons />
+      <CountRenderer />
+    </>
+  );
 }
 
-function Buttons(){
-  const [count,setCount] = useRecoilState(count_State)
-    // the button is still renrendering but it should not , the only thing changing is count value which 
-    //which is changing in CountRenderer
-  return(<>
-     <button onClick={()=>{setCount(count+1)}} >Increament</button>
-  </>)
+function Buttons() {
+  const setCount = useSetRecoilState(count_State);
+  // now it is nit re-rendering
+  return (
+    <>
+      <button
+        onClick={() => {
+          setCount(count=>count + 1);
+        }}
+      >
+        Increament
+      </button>
+    </>
+  );
 }
 
-function CountRenderer(){
-  
-  const count = useRecoilValue(count_State)
+function CountRenderer() {
+  const count = useRecoilValue(count_State);
 
-  return(<>
+  return (
+    <>
       <h1>{count}</h1>
-  </>)
+    </>
+  );
 }
 
-
-export default App
+export default App;
