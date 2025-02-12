a0e9834b5145d85dc3e5bb3c5b4fe012d68df9cb
Yashraj360 <yashraj.sahu.jobsdoor360@gmail.com>
2025-02-13 01:42:19 +0530

    useRecoilValueLodable

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilValueLoadable } from "recoil";
 import "./App.css";
 import { todo } from "./Store/Atoms/TodoAtomFamily"; 
 
@@ -13,34 +13,33 @@ function App() {
 
 function Todo({ id }) { 
 
-  const todoItem = useRecoilValue(todo(id)); 
+  const todo = useRecoilValueLoadable(todo(id)); 
+    
+  if(todo.state === "loading"){
+    return <div>Loading...</div>;
+  }
+  else if (todo.state === "hasValue"){
+    return (
+      <>
+        <h1>{todo.contents.title}</h1>
+        <h2>{todo.contents.description}</h2>
+      </>
+    );
+  }
+  else if (todo.state === "hasError"){
+    return <div>Error</div>;
+  }
   
-  return (
-    <>
-      <h1>{todoItem.title}</h1>
-      <h2>{todoItem.description}</h2>
-    </>
-  );
 }
 
 export default App;
 
+//  useRecoilValueLoadable  are hooks used to interact with Recoil loadable states. Loadable states are a special type of state that can represent asynchronous operations, such as fetching data. These hooks help manage the loading, error, and success states of asynchronous data.
 
-// selectorFamily in Recoil
-// selectorFamily is a powerful feature in Recoil that allows you to create dynamic selectors based on a parameter. It helps to derive state or compute values from atoms or other selectors, while also allowing for unique instances based on input.
+// useRecoilValueLoadable
+// useRecoilValueLoadable is used to read the current value of a Recoil atom or selector, but also allows you to handle the loading and error states.
 
-// Key Features of selectorFamily:
-// Dynamic Selection: Creates selectors that can accept parameters, allowing you to derive different values based on input.
-// Async Capabilities: Supports asynchronous fetching of data, which is useful for data fetching scenarios (e.g., API calls).
-// Computation: Can compute derived state based on other atoms or selectors..
-
-
-// How It Works
-// Dynamic Parameter: The userSelector can be called with different user IDs (e.g., userSelector(1), userSelector(2)) to fetch user data dynamically.
-// Fetching Data: Each call creates a new selector instance that fetches data based on the provided user ID.
-// Reactivity: If the user ID changes, the component automatically re-renders with the new user data.
-
-// When to Use selectorFamily?
-// Dynamic Fetching: When you need to fetch or compute data based on changing parameters.
-// Modular Code: When you want to keep your code modular by creating reusable selectors.
-// Async Operations: When you need to perform asynchronous data fetching.
+// Returns: A Loadable object, which has three possible states:
+// loading: The data is still being fetched.
+// hasValue: The data has been successfully fetched.
+// hasError: There was an error while fetching the data.
